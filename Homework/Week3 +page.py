a459bb3e3f876213b26ef90d74479186f09b07de
MinYim <MinYim@users.noreply.github.com>
2020-05-24 18:55:39 +0900

    Week3 HW
    Page 변환은 +page 파일의 range() 로 구분함

---
+++ Homework/Week3 +page.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+rank = 0
+for page in range(2):
+    data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg='+str(page + 1),headers=headers)
+    
+    soup = BeautifulSoup(data.text, 'html.parser')
+    songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')    
+    for song in songs:
+        title = song.select_one('td.info > a.title.ellipsis')
+        artist = song.select_one('td.info > a.artist.ellipsis')
+        rank += 1
+        print(rank, title.text.strip(), artist.text)
